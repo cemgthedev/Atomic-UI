e88d29a6fb9b245dcb10cd265d7bf9639352d777
cemgthedev <cemg.the.dev@gmail.com>
2026-05-11 19:57:21 -0300

    build: install vite with react and typescript and config tailwind

---
+++ app/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="bg-zinc-800 h-screen flex justify-center items-center">
+      <h1 className="text-gray-50 text-3xl">Get started!!</h1>
+    </main>
+  );
+}
+
+export default App;
