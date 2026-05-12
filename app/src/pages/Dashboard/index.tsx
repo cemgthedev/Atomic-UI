5aa4fefb6c83f667b1772468b3f65225d2e09a09
cemgthedev <cemg.the.dev@gmail.com>
2026-05-12 11:30:06 -0300

    feat: add heading and shadcn
    - add and configure shadcn
    - add heading component

--- app/src/pages/Dashboard/index.tsx
+++ app/src/pages/Dashboard/index.tsx
@@ -1,10 +1,13 @@
-import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
 
 export function Dashboard() {
   return (
-    <main className="bg-zinc-800 h-screen flex flex-col justify-center items-center gap-4">
-      <h1 className="text-gray-50 text-3xl">Get started!!</h1>
-      <Button />
+    <main className="h-screen flex flex-col justify-center items-center gap-4">
+      <Heading size={"xl"}>Get started!!</Heading>
+      <Heading size={"lg"} variant={"success"}>Get started!!</Heading>
+      <Heading size={"md"} variant={"warning"}>Get started!!</Heading>
+      <Heading size={"sm"} variant={"danger"}>Get started!!</Heading>
+      <Heading size={"xs"}>Get started!!</Heading>
     </main>
   );
 }
